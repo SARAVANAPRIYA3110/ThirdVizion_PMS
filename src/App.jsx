a9a5c53e6fb89c8e61a9005fd2c00514b1489b16
SARAVANAPRIYA3110 <saravanapriya31102004@gmail.com>
2025-10-25 16:23:15 +0530

    admin

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@ import { useState } from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Admin from './Admin';
 import Login from './Login';
+import Project from './AdminDashBoard/Project';
+import Users from './AdminDashBoard/Users';
+import Report from './AdminDashBoard/Report';
+import Teams from './AdminDashBoard/Teams';
 
 
 // import reactLogo from './assets/react.svg'
@@ -19,6 +23,10 @@ function App() {
         <Route path="/" element={<h1>Hello World</h1>} />
         <Route path="/admin" element={<Admin />} />
         <Route path='/login' element={<Login />} />
+        <Route path='/project' element={<Project />} />
+        <Route path='/Users' element={<Users />} />
+        <Route path='/Report' element={<Report />} />
+        <Route path='/teams' element={<Teams />} />
       
       </Routes>
     </BrowserRouter>
