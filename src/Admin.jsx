9cd0c8b09aeb975c63458623bee8d86a7777f475
SARAVANAPRIYA3110 <saravanapriya31102004@gmail.com>
2025-10-25 11:58:43 +0530

    first commit

---
+++ src/Admin.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Admin = () => {
+  return (
+    <div>
+      <h1>Admin Dahboard</h1>
+    </div>
+  )
+}
+
+export default Admin
---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Admin from './Admin';
+
+
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<h1>Hello World</h1>} />
+        <Route path="/admin" element={<Admin />} />
+      
+      </Routes>
+    </BrowserRouter>
+    </>
+  )
+}
+
+export default App
